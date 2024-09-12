0960350ebf218247649e1ee3da3ac7b8b0d045d8
Luis-Guillermo-Rivera-Stephens <148508811+Luis-Guillermo-Rivera-Stephens@users.noreply.github.com>
2024-09-11 22:23:12 -0600

    Practica 2, comentarios

--- practica1/practica2/hilos.c
+++ practica1/practica2/hilos.c
@@ -5,13 +5,14 @@
 
 
 double area = 0.0;
-pthread_mutex_t mutex;  
+pthread_mutex_t mutex;  //mutex para que los hilos no escriban a la vez en la variable de area
 
-double raiz(double x, double y) {
+double raiz(double x, double y) {   //Esta funcion va a calcular la altura del punto x que usamos, porque 
+                                    //R^2 = y^2 + x^2 y si despejamos j nos da una funcion de x
     return sqrt(y * y - x * x);
 }
 
-typedef struct {
+typedef struct {        //Los argumentos que va a usar cada hilo para que pueda hacer el calculo desde afuera
     double a;
     double h;
     double radio;
@@ -27,7 +28,8 @@ void* calcular_trapecio(void* args) {
     area += resultado;
     pthread_mutex_unlock(&mutex);
 
-    pthread_exit(NULL);
+    pthread_exit(NULL);     //Hace la operacion con cada n dada, donde le va a sumar la altura al area para una vez 
+                            //terminada la ejecucion, lo multiplique por la base
 }
 
 double sistema_de_trapecios(double radio, int n) {
@@ -35,10 +37,13 @@ double sistema_de_trapecios(double radio, int n) {
     double b = radio;
     double h = (b - a) / n;
 
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex); //MUTEX PARA LOCKEAR LA VARIABLE GLOBAL AREA 
+    //segun la regla de los trapecios se tienen que sumar la primera
+    //y la ultima altura primero
     area = raiz(a, radio) + raiz(b, radio);
     pthread_mutex_unlock(&mutex);
 
+    
     pthread_t hilos[n];  
     HiloArgs args[n];  
     HiloArgs aux;
@@ -49,16 +54,17 @@ double sistema_de_trapecios(double radio, int n) {
         aux.radio = radio;
         aux.i = i;
 
-        args[i] = aux;
-        pthread_create(&hilos[i], NULL, calcular_trapecio, (void*)&args[i]);
+        args[i] = aux; //creacion de los argumentos para los hilos, 
+        pthread_create(&hilos[i], NULL, calcular_trapecio, (void*)&args[i]);  //Creacion de hilos por cada iteracion
+            // Se supone que solo son 4 cores pero esto es mas escalable en caso de querer correrlo en mas cores
     }
 
     for (int i = 1; i < n; i++) {
-        pthread_join(hilos[i], NULL);
+        pthread_join(hilos[i], NULL); //espera a que todos los hilos terminen para poder multiplicar todo por la base
     }
 
     pthread_mutex_lock(&mutex);
-    area = (h / 2) * area;
+    area = (h / 2) * area; //se multiplica toda la sumatoria por la base
     pthread_mutex_unlock(&mutex);
 
     return area;
@@ -69,7 +75,7 @@ int main() {
     int n;
     double resultado;
 
-    pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&mutex, NULL); //se inicializa el mutex
 
     printf("Introduce el radio que desees utilizar: ");
     scanf("%lf", &radio);
--- practica1/practica2/nohilos.c
+++ practica1/practica2/nohilos.c
@@ -2,24 +2,25 @@
 #include <math.h>
 #include <time.h> 
 
-double raiz(double x, double y){ 
+double raiz(double x, double y){  //Esta funcion va a calcular la altura del punto x que usamos, porque 
+                                  //R^2 = y^2 + x^2 y si despejamos j nos da una funcion de x
     return sqrt(y*y - x*x);
 }
 
-double sistema_de_trapecios(double radio, int n){
+double sistema_de_trapecios(double radio, int n){ //calculara el area de todos los trapecios
     double area = 0.0;
     double a = 0.0;
     double b = radio;
     double h = (b - a) / n;
 
-    area = raiz(a, radio) + raiz(b, radio);
-
+    area = raiz(a, radio) + raiz(b, radio); //segun la regla de los trapecios se tienen que sumar la primera
+                                            //y la ultima altura primero
     for (int i = 1; i < n; i++) { 
         double x_i = a + i * h; 
-        area += 2 * raiz(x_i, radio);  
+        area += 2 * raiz(x_i, radio);       //calcula la altura de cada x que le enviamos  y la suma al area
     }
 
-    area = (h / 2) * area; 
+    area = (h / 2) * area;                  //multiplica todas las alturas por la base, pues xa + xb +xc = x(a + b + c)
 
     return area;
 }
@@ -29,6 +30,7 @@ int main(){
     int n;
     double resultado;
 
+    //variables para contabilizar el tiempo 
     clock_t start_time, end_time;
     double cpu_time_used;
 
@@ -38,11 +40,12 @@ int main(){
     scanf("%d", &n);
 
     start_time = clock();
-    resultado = sistema_de_trapecios(radio, n);
+    resultado = sistema_de_trapecios(radio, n); //inicio de la funcion
     end_time = clock();
     
     cpu_time_used = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
 
+    //resultados 
     printf("Esta es el área del cuarto de círculo: %f\n", resultado);
     printf("Esta es el área del círculo completo: %f\n", resultado * 4);
     printf("Tiempo de ejecución: %f segundos\n", cpu_time_used);
